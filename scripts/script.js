2ba699c33f6e2512f65a5a2d0d357ecf894d0476
LukSiman <simanavicius.lukas@gmail.com>
2021-09-02 22:01:31 -0700

    implementing winner checking functionality

--- scripts/script.js
+++ scripts/script.js
@@ -1,18 +1,18 @@
 // Gameboard object constructor
 const Gameboard = (() => {
-    const gameBoard = [['', '', ''], ['', '', ''], ['', '', '']];
+    const gameBoard = new Array(9);
 
     const board = document.querySelector('#board');
     for (let i = 0; i < gameBoard.length; i++) {
-        for (let j = 0; j < gameBoard[i].length; j++) {
             const square = document.createElement('button');
             square.className = 'square';
             board.appendChild(square);
-        }
     }
 
     return { gameBoard }
 })();
+
+const gameBoard = Gameboard;
 
 // Game constroller constructor
 const Controller = (() => {
@@ -36,10 +36,39 @@ const Controller = (() => {
                 }
                 markChecker();
                 square.innerHTML = currentMark;
-
-                
+                updateArray();
+                checkWinner();               
             })
         })
+    }
+
+    const playBoard = gameBoard.gameBoard;
+
+    const updateArray = () => {
+        const board = document.querySelector('#board').children;
+        let i = 0;
+        for(playSquare of board){     
+            if(playSquare.innerHTML === 'X'){
+                playBoard[i] = 1;
+            } else if (playSquare.innerHTML === 'O'){
+                playBoard[i] = 0;
+            }
+            i++;
+        }
+    }    
+
+    const checkWinner = () => {
+        if( playBoard[0] + playBoard[1] + playBoard[2] === 3 ||
+            playBoard[0] + playBoard[3] + playBoard[6] === 3 ||
+            playBoard[0] + playBoard[4] + playBoard[8] === 3 ||
+            playBoard[1] + playBoard[4] + playBoard[7] === 3 ||
+            playBoard[2] + playBoard[4] + playBoard[6] === 3 ||
+            playBoard[2] + playBoard[5] + playBoard[8] === 3 ||
+            playBoard[3] + playBoard[4] + playBoard[5] === 3 ||
+            playBoard[6] + playBoard[7] + playBoard[8] === 3 
+            ){
+            console.log('X wins!');
+        }
     }
 
     return { addMarks }
@@ -50,7 +79,7 @@ const Player = (name) => {
     return { name }
 };
 
-const gameBoard = Gameboard;
+
 const playerOne = Player('Bob');
 const playerTwo = Player('Lukas');
 const controller = Controller;
